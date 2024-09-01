290f0b0b1c9e395e2d2a4d1f1d3d33aca6e89662
Robert He <2567466856@qq.com>
2024-09-01 20:38:09 +0800

    Initial Commit

---
+++ CMD-Video-Player/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CMD-Video-Player
+//
+//  Created by Robert He on 2024/9/1.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
